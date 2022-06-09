7f319fb40a8fe4982a9b416d98c69a15890b220a
Bilbagbobins <aaronxt@hotmail.com>
2022-06-09 20:04:19 +1200

    implement basic functionality

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Calculator
+This is the Calculator assignment for The Odin Project.
+
+We are required to build a basic functional calculator. It needs to be able to perform addition, subtraction, multiplication and division. A few things it is required to:
+
+1. String together several operations
+2. It should not evaluate more than one single pair of numbers at a time
+3. Any decimals should not overflow the screen
+4. A clear button should wipe out any existing data
+5. Display a snarky message if the user attempts to divide by 0
+
+Extra credit for implementing any of the following:
+
+1. Add a decimal input option which only accepts one decimal
+2. Add a backspace button to allow the user to undo an accidental button
+3. Add keyboard support
+4. Make it look pretty and distinct
--- calculator.js
+++ calculator.js
@@ -7,6 +7,7 @@ function subtract(a, b) {
 }
 
 function divide(a, b) {
+    if (b == 0) return;
     return a / b;
 }
 
@@ -15,9 +16,41 @@ function multiply(a, b) {
 }
 
 function operate(operator, a, b) {
-    return operator(a, b);
+    // using 'window' to allow a variable to be passed as a function call
+    let num = window[operator](parseFloat(a), parseFloat(b))
+    display.textContent = num;
+    displayValue = '';
 }
+
+function operatorValue(){
+    operator = this.id;
+    num1 = displayValue;
+    displayValue = '';
+}
+
+function displayItem() {
+    if (displayValue.length > 9) return;
+    displayValue += String(this.textContent);
+    display.textContent = displayValue;  
+}
+
+let num1 = '';
+let operator = '';
+let displayValue = '';
+const display = document.querySelector('.display');
 const numbers = document.querySelector('.numbers');
+const equals = document.querySelector('#equals')
+equals.addEventListener('click', () => {
+    if (num1 && operator && displayValue) {
+        operate(operator, num1, displayValue)
+    }
+});
+
+const addButton = document.querySelector('#add').addEventListener('click', operatorValue);
+const subtractButton = document.querySelector('#subtract').addEventListener('click', operatorValue);
+const multiplyButton = document.querySelector('#multiply').addEventListener('click', operatorValue);
+const divideButton = document.querySelector('#divide').addEventListener('click', operatorValue);
+
 
 for (i = 0; i < 10; i++) {
     const num = document.createElement('button');
@@ -25,5 +58,7 @@ for (i = 0; i < 10; i++) {
     num.classList.add('number');
     num.setAttribute('id', i);
     numbers.appendChild(num);
+    const button = document.querySelectorAll('.number');
+    button.forEach(number => number.addEventListener('click', displayItem))
 }
 
