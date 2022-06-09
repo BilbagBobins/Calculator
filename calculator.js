0ec1c0c10dd75ab738825b37b3538909adacc88a
Bilbagbobins <aaronxt@hotmail.com>
2022-06-09 17:07:09 +1200

    add buttons

--- calculator.js
+++ calculator.js
@@ -14,3 +14,16 @@ function multiply(a, b) {
     return a * b;
 }
 
+function operate(operator, a, b) {
+    return operator(a, b);
+}
+const numbers = document.querySelector('.numbers');
+
+for (i = 0; i < 10; i++) {
+    const num = document.createElement('button');
+    num.textContent = i;
+    num.classList.add('number');
+    num.setAttribute('id', i);
+    numbers.appendChild(num);
+}
+
